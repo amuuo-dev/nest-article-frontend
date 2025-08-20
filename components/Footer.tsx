2d7d35bcbb090435dcd2b105c4d459469cc40644
muuo <anthonymuuo92@gmail.com>
2025-08-20 15:21:22 +0300

    added a menu bar on small screens

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,26 +4,30 @@ import { Button } from "./ui/button";
 const Footer = () => {
   return (
     <div className="">
-      <div className="py-8 px-12 flex justify-between">
+      <div className="md:py-8 md:px-12 py-4 px-6 flex justify-between">
         <div>
           <Link href="/">
-            <h1 className="text-2xl font-semibold hover:bg-amber-100 px-2 py-1 rounded-md cursor-pointer">
+            <h1 className="md:text-2xl text-lg font-semibold hover:bg-amber-100 px-2 py-1 rounded-md cursor-pointer">
               <span className="">Arti</span>
               <span className="text-amber-500">Zone</span>
             </h1>
           </Link>
         </div>
         <div>
-          <h1 className="font-semibold hover:underline cursor-pointer">
+          <h1 className="font-semibold hover:underline cursor-pointer text-base md:text-xs">
             Terms and Conditions
           </h1>
-          <h1 className="font-semibold my-4">Start you're account Today!</h1>
-          <Button className="border border-amber-300 px-8 cursor-pointer">
-            Sign Up
-          </Button>
+          <h1 className="font-semibold md:my-4 my-2 md:text-base text-xs">
+            Start you're account Today!
+          </h1>
+          <Link href="/signup">
+            <Button className="border border-amber-300 px-8 cursor-pointer">
+              Sign Up
+            </Button>
+          </Link>
         </div>
       </div>
-      <h1 className="text-center font-semibold my-6">
+      <h1 className="text-center font-semibold md:my-6 my-3 md:text-base text-xs">
         Built by{" "}
         <Link
           href="https://www.linkedin.com/in/anthony-muuo/"
@@ -31,7 +35,7 @@ const Footer = () => {
         >
           Anthony Muuo
         </Link>
-        . The source code is available on{" "}
+        . code is available on{" "}
         <Link
           href="https://github.com/amuuo-dev"
           className="underline text-amber-700 cursor-pointer"
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,18 @@
 import { Button } from "./ui/button";
 import Link from "next/link";
+import { Menu } from "lucide-react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
 
 const Header = () => {
   return (
-    <div className="bg-white py-8 px-12">
+    <div className="bg-white md:py-8 md:px-12 py-4 px-6">
       <header className="flex justify-between">
         <Link href="/">
           <h1 className="text-2xl font-semibold hover:bg-amber-100 px-2 py-1 rounded-md cursor-pointer">
@@ -18,9 +27,39 @@ const Header = () => {
               Login
             </Button>
           </Link>
-          <Button className="hover:bg-amber-100 px-12 py-2 rounded-md cursor-pointer flex-1 border border-amber-300 hover:text-amber-500">
-            Sign Up
-          </Button>
+          <Link href="/signup">
+            <Button className="hover:bg-amber-100 px-12 py-2 rounded-md cursor-pointer flex-1 border border-amber-300 hover:text-amber-500">
+              Sign Up
+            </Button>
+          </Link>
+        </div>
+        {/* mobile */}
+        <div className="md:hidden">
+          <Sheet>
+            <SheetTrigger asChild>
+              <Menu />
+            </SheetTrigger>
+            <SheetContent className="bg-[#FCF8FF]">
+              <SheetHeader>
+                <SheetTitle>Menu</SheetTitle>
+                <SheetDescription>
+                  Navigate around ArtiZone Pages
+                </SheetDescription>
+              </SheetHeader>
+              <div className="flex flex-col gap-4 items-center justify-center">
+                <Link href="/login" className="w-full px-2">
+                  <Button className="w-full hover:bg-amber-100 px-12 py-2 rounded-md cursor-pointer flex-1 border border-amber-300 hover:text-amber-500">
+                    Login
+                  </Button>
+                </Link>
+                <Link href="/signup" className="w-full px-2">
+                  <Button className="w-full hover:bg-amber-100 px-12 py-2 rounded-md cursor-pointer flex-1 border border-amber-300 hover:text-amber-500">
+                    Sign Up
+                  </Button>
+                </Link>
+              </div>
+            </SheetContent>
+          </Sheet>
         </div>
       </header>
     </div>
